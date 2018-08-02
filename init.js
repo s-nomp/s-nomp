6d529df2557eb33654eddd0b7df2b1d348aaf0b3
punkoivan <punkoivan@gmail.com>
2018-08-02 17:05:16 -0400

    added password parameter and authorization

--- init.js
+++ init.js
@@ -203,6 +203,7 @@ var spawnPoolWorkers = function(){
         } else if (!connection) {
             redisConfig = pcfg.redis;
             connection = redis.createClient(redisConfig.port, redisConfig.host);
+            connection.auth(redisConfig.password);
             connection.on('ready', function(){
                 logger.debug('PPLNT', coin, 'TimeShare processing setup with redis (' + redisConfig.host +
                     ':' + redisConfig.port  + ')');
