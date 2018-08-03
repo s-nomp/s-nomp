352bf19c33ab8aa7f871f77403b01fc04e9b126a
PunkoIvan <punkoivan@gmail.com>
2018-08-03 12:03:40 +0300

    Add authorization for redis in init.js (#27)
    This adds missing authentication for redis connection initialization in init.js.

--- init.js
+++ init.js
@@ -203,6 +203,12 @@ var spawnPoolWorkers = function(){
         } else if (!connection) {
             redisConfig = pcfg.redis;
             connection = redis.createClient(redisConfig.port, redisConfig.host);
+            if (redisConfig.password != "") {
+                connection.auth(redisConfig.password);
+                connection.on("error", function (err) {
+                    logger.error("redis", coin, "An error occured while attempting to authenticate redis: " + err);
+                });
+            }
             connection.on('ready', function(){
                 logger.debug('PPLNT', coin, 'TimeShare processing setup with redis (' + redisConfig.host +
                     ':' + redisConfig.port  + ')');
