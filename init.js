3f7f3a2315850e9e2e1615cedc0997c39d033feb
punkoivan <punkoivan@gmail.com>
2018-08-03 04:58:46 -0400

    added error handling, logging

--- init.js
+++ init.js
@@ -203,7 +203,12 @@ var spawnPoolWorkers = function(){
         } else if (!connection) {
             redisConfig = pcfg.redis;
             connection = redis.createClient(redisConfig.port, redisConfig.host);
-            connection.auth(redisConfig.password);
+            if (redisConfig.password != "") {
+                connection.auth(redisConfig.password);
+                connection.on("error", function (err) {
+                    logger.error("redis", coin, "An error occured while attempting to authenticate redis: " + err);
+                });
+            }
             connection.on('ready', function(){
                 logger.debug('PPLNT', coin, 'TimeShare processing setup with redis (' + redisConfig.host +
                     ':' + redisConfig.port  + ')');
