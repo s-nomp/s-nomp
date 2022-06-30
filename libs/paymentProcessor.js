e4b7bf3854ce0d6a52999d2c21d93b1d0c41a114
Seal-Clubber <35927816+Seal-Clubber@users.noreply.github.com>
2022-06-30 07:05:18 +0200

    Update paymentProcessor.js

--- libs/paymentProcessor.js
+++ libs/paymentProcessor.js
@@ -188,7 +188,7 @@ function SetupForPool(logger, poolOptions, setupFinished){
     if (requireShielding === true) {
         async.parallel([validateAddress, validateTAddress, validateZAddress, getBalance], asyncComplete);
     } else {
-        async.parallel([validateAddress, validateTAddress, getBalance], asyncComplete);
+        async.parallel([validateAddress, getBalance], asyncComplete);
     }
 
     //get t_address coinbalance
